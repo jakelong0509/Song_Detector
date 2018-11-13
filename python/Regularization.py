91414d31cdd479f11ff03eba18a738a5f93220dd
jakelong0509 <jakelong0509@gmail.com>
2018-11-13 10:21:21 -0500

    add Regularization

---
+++ python/Regularization.py
@@ -0,0 +1,14 @@
+import numpy as np
+import os
+import sys
+
+class regularization():
+    def __init__(self, hidden_state):
+        # shape of hidden_state = (Tx, 2 * n_a)
+        self.hidden_state = hidden_state
+
+    def dropout(self, ):
+        shape = self.hidden_state.shape
+        eye = np.eye(shape)
+
+    def normalization(self):
