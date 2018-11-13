1e24fc07c868234d55e320e50d182ff528d907f8
jakelong0509 <jakelong0509@gmail.com>
2018-11-13 12:02:00 -0500

    Add Attention Model

---
+++ python/attention_model.py
@@ -0,0 +1,37 @@
+import numpy as np
+from functions import helper_func as func
+
+class attention_model():
+    def __init__(self, _current_A, _prev_s, layer_dimension):
+        """
+        Attention Model at time step t in Ty
+        -----Parameter------
+        _current_A: hidden state of bi-Directional LSTM from time step 1 to S... 2 to S + 1.... (sliding windows) (S, n_a)
+        _prev_s: hidden state of post-LSTM from time step t-1 in Ty (1, n_s)
+        layer_dimsion: dimension of hidden layer (type = list)
+        -----Return-------
+        _c_t: context at time-step t in Ty
+        """
+        self._layer = None
+        # checking if layer_dimension argument is a list or not
+        if isinstance(layer_dimension, list):
+            self._layer = layer_dimension
+        else:
+            sys.exit("The argument of layer_dimension is not a list. Terminate operation")
+
+        self._current_A = _current_A
+        self._prev_s = _prev_s
+        self.S = _current_A.shape[0]
+        self.n_s = _prev_s.shape[1]
+
+        # call duplicate function fron functions module to duplicate _prev_s from (1,n_s) to (S, n_s)
+        self._prev_S = func.duplicate(self.S, self.n_s, self._prev_s, axis = 0)
+
+        # concatenate S and A
+        self._SA_concat = np.concatenate((self._current_A, self._prev_S), axis = 0)
+
+        # assert the dimension of concatenate
+        assert(self._SA_concat.shape == (self.S, self._current_A.shape[1] + self.n_s))
+
+    def cell_forward(self):
+        
--- python/functions/helper_func.py
+++ python/functions/helper_func.py
@@ -16,3 +16,17 @@ def xavier(dimension):
     n_out, n_in = dimension
     limit = np.sqrt(6 / (n_in + n_out))
     return np.random.uniform(-limit, limit, size = dimension)
+
+# duplicate hidden state
+def duplicate(S, n_s, _prev_s, axis):
+    # Parameter:
+    # S: width of windows
+    # n_s: dimension of hidden state of post_LSTM
+    _prev_S = None
+    if axis == 0:
+        ones = np.ones((S, n_s))
+        _prev_S = ones * _prev_s
+    elif axis == 1:
+        ones = np.ones((n_s, S))
+        _prev_S = ones * _prev_s
+    return _prev_S
