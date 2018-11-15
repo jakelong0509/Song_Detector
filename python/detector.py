0f7bbdda5155a7c2958668f30855766a04be8877
jakelong0509 <jakelong0509@gmail.com>
2018-11-15 15:49:19 -0500

    update detector model

--- python/detector.py
+++ python/detector.py
@@ -5,12 +5,13 @@ import sys
 from LSTM import LSTM
 from wrapper import Bidirectional
 from Regularization import regularization
-from attention_model import *
+from attention_model import attention_model
 from data_preprocessing import song_preprocessing
 from functions import activations as act, helper_func as func
 
 if __name__ == "__main__":
     sec = 5
+    jump_step = 100
     main_dir = os.getcwd()
 
     # change directory to get songs
@@ -20,13 +21,13 @@ if __name__ == "__main__":
     # get Tx
     Tx, duration = song_preprocessing.get_Tx(songs_dir + "/")
     sec_per_spec = duration / Tx
-    S = np.round(sec / sec_per_spec)
+    S = int(np.round(sec / sec_per_spec))
 
     # return back to main directory
     os.chdir(main_dir)
 
     # get Ty
-    Ty = song_preprocessing.get_Ty(Tx, S)
+    Ty = song_preprocessing.get_Ty(Tx, S, jump_step)
 
     # preprocessing data X.shape = (m, Tx, n_x) | Y.shape = (m, Ty, n_y)
     X, Y = song_preprocessing.preprocessing_data(songs_dir + "/", Tx, Ty)
@@ -37,5 +38,34 @@ if __name__ == "__main__":
 
     pre_LSTM = LSTM((Tx, n_x), (Tx, n_a))
     pre_bi_LSTM = Bidirectional(pre_LSTM, X[0,:,:])
-    A = pre_bi_LSTM.concatLSTM()
+    A = pre_bi_LSTM.concatLSTM() # shape = (Tx, 2 * n_a)
+
+    # TODO: dropout A
+
+    # attention and post_LSTM
+    n_s = 256
+    n_c = n_a
+    post_LSTM = LSTM((Ty, n_c), (Ty, n_s))
+    start = 0
+    end = S
+    prev_s = np.zeros((1, n_s))
+    prev_a = np.zeros((1, n_s))
+    hidden_dimension = [128, 64]
+    lstm_S = []
+    attentions = []
+    for t in range(Ty):
+        attention = attention_model(A[start:end,:], prev_s, hidden_dimension)
+        attentions.append(attention)
+        start = start + jump_step
+        end = end + jump_step
+
+        alphas, c, _energies, _caches_t = attention.nn_forward_propagation()
+        st, at, cache = post_LSTM.cell_forward(prev_s, prev_a, c)
+        lstm_S.append(st)
+        prev_s = st
+        prev_a = at
+    lstm_S = lstm_S.reshape(-1)
+    # TODO: dropout lstm_S
+
+    # initialize last layer Wy
     print(A.shape)
